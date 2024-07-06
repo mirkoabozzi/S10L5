c2b7fddc26a28f7a8e816bbcccf5ff739989dd5e
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-06 14:02:14 +0200

    style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,15 +17,19 @@ const Home = () => {
 
   const navigate = useNavigate();
 
+  const upperCase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
   const tempToC = (value) => (value - 273).toFixed(1);
-  const speedToKm = (value) => (value * 3.6).toFixed(2);
+  const speedToKm = (value) => (value * 3.6).toFixed(1);
   const dataConverter = (timeStamp) => {
     const data = new Date(timeStamp * 1000);
     return data.toLocaleString("en-En", {
+      // weekday: "long",
       year: "numeric",
       month: "long",
       day: "numeric",
       hour: "numeric",
+      // minute: "numeric",
     });
   };
 
@@ -114,7 +118,7 @@ const Home = () => {
               <Row className="justify-content-around">
                 <Col sm="4" className="mb-4 text-center">
                   <p className="mb-0">Feels Like</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-thermometer-half me-2" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-thermometer-half me-2" viewBox="0 0 16 16">
                     <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415" />
                     <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1" />
                   </svg>{" "}
@@ -122,7 +126,7 @@ const Home = () => {
                 </Col>
                 <Col sm="4" className="mb-4 text-center">
                   <p className="mb-0">Wind Speed</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-wind me-2" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-wind me-2" viewBox="0 0 16 16">
                     <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5" />
                   </svg>
                   {speedToKm(weather.wind.speed)} km/h
@@ -131,14 +135,14 @@ const Home = () => {
               <Row className="justify-content-around">
                 <Col sm="4" className="mb-4 text-center">
                   <p className="mb-0">Precipitation</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-cloud me-2" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cloud me-2" viewBox="0 0 16 16">
                     <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383m.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
                   </svg>
-                  {weather.weather[0].description}
+                  {upperCase(weather.weather[0].description)}
                 </Col>
                 <Col sm="4" className="mb-4 text-center">
                   <p className="mb-0">Umidity</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-droplet" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-droplet" viewBox="0 0 16 16">
                     <path
                       fillRule="evenodd"
                       d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"
